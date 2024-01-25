65932c6b60983325ad991a0ee2d0c72f73e0074e
Julian Wälde <julian.waelde@hs-rm.de>
2024-01-25 12:42:40 +0100

    add shell.nix/.envrc for development environment

---
+++ shell.nix
@@ -0,0 +1,59 @@
+{ pkgs ? import (if pinned-nixpkgs then scripts/nixpkgs.nix else <nixpkgs>) {}
+, inCI ? false
+, pinned-nixpkgs ? inCI
+, coqDeps ? !inCI
+, ocamlDeps ? !inCI
+, testDeps ? !inCI
+, devTools ? !inCI
+, ecRef ? ""
+, opamDeps ? false
+, enableFramePointers ? false
+}:
+
+with pkgs;
+
+let inherit (lib) optionals; in
+
+let coqPackages = coqPackages_8_18; in
+
+let mathcomp-word = callPackage scripts/mathcomp-word.nix { inherit coqPackages; }; in
+
+let easycrypt = callPackage scripts/easycrypt.nix {
+  inherit ecRef;
+  why3 = pkgs.why3.overrideAttrs (o: {
+      configureFlags = o.configureFlags ++ [ "--disable-ide" ];
+    });
+}; in
+
+let inherit (coqPackages.coq) ocamlPackages; in
+
+let oP =
+  if enableFramePointers
+  then ocamlPackages.overrideScope' (self: super: {
+    ocaml = super.ocaml.overrideAttrs (o: {
+      configureFlags = o.configureFlags ++ [ "--enable-frame-pointers" ];
+    });
+  })
+  else ocamlPackages
+; in
+
+if !lib.versionAtLeast oP.ocaml.version "4.11"
+then throw "Jasmin requires OCaml ≥ 4.11"
+else
+
+let ecDeps = ecRef != ""; in
+
+pkgs.mkShell {
+  buildInputs = []
+    ++ optionals coqDeps [ coqPackages.coq mathcomp-word ]
+    ++ optionals testDeps ([ curl.bin oP.apron.out libllvm ] ++ (with python3Packages; [ python pyyaml ]))
+    ++ optionals ocamlDeps ([ mpfr ppl ] ++ (with oP; [
+         ocaml findlib dune_3
+         cmdliner
+         batteries
+         menhir (oP.menhirLib or null) zarith camlidl apron yojson ]))
+    ++ optionals devTools (with oP; [ merlin ocaml-lsp ])
+    ++ optionals ecDeps [ easycrypt alt-ergo z3.out ]
+    ++ optionals opamDeps [ rsync git pkg-config perl ppl mpfr opam ]
+    ;
+}
